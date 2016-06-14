471d615b8483c47fb1c66ec6b193e6118ae875c4
Xin Chen <chenx26@uw.edu>
2016-06-14 09:51:37 -0600

    Improved Documentation

--- R/hello.R
+++ R/hello.R
@@ -1,12 +1,13 @@
 
-#' Title1
+#' Print "Hello, name"
 #'
-#' @param name
+#' @param name The name to say hello to
 #'
-#' @return
+#' @return None
 #' @export
 #'
 #' @examples
+#' hello("world")
 hello.name <- function(name) {
   print(paste("hello,",name))
 }
@@ -14,12 +15,13 @@ hello.name <- function(name) {
 
 #' Print "Greetings, yourname"
 #'
-#' @param yourname
+#' @param yourname The name to greet
 #'
-#' @return
+#' @return None
 #' @export
 #'
 #' @examples
+#' hello2("world")
 hello2=function(yourname){
   print(paste("Greetings,",yourname))
 }
