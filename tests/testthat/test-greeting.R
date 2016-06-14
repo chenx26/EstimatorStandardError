58cde0da045e751b2b25f5014e2c5617370205d6
Xin Chen <chenx26@uw.edu>
2016-06-14 10:21:59 -0600

    Added test for hello2

--- tests/testthat/test-greeting.R
+++ tests/testthat/test-greeting.R
@@ -3,4 +3,5 @@ library(EstimatorStandardError)
 context("Test Greeting Function")
 
 test_that("The proper name is greeted",
-          expect_equal(hello.name("abc"),"hello, abc"))
+          expect_equal(hello.name("abc"),"hello, abc"),
+          expect_equal(hello2("abc"),"Greetings, abc"))
