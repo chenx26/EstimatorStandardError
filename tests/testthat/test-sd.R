11b6e072da604f35813330aca5077ac61d0d87d3
Xin Chen <chenx26@uw.edu>
2016-06-23 14:27:35 -0600

    Renamed test-greeting to tets-sd and changed the test contents

--- tests/testthat/test-sd.R
+++ tests/testthat/test-sd.R
@@ -1,7 +1,11 @@
 library(EstimatorStandardError)
 
-context("Test Greeting Function")
+context("Test Computing Standard Deviation")
 
 # test_that("The proper name is greeted",{
 #           expect_equal(hello.name("abc"),"hello, abc")
 #           expect_equal(hello2("abc"),"Greetings, abc")})
+
+test_that("The standard deviation of constant time series is zero",
+          expect_eual(sd(rep(1,5)),0))
+
