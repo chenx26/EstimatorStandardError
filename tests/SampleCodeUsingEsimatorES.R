9289d47f64ae527fdd5f4c5a1239ce1b52e82fb9
Xin Chen <chenx26@uw.edu>
2017-09-20 15:36:33 -0700

    Sample code for using the standard error functions and printing them.

---
+++ tests/SampleCodeUsingEsimatorES.R
@@ -0,0 +1,64 @@
+rm(list=ls())
+library(EstimatorStandardError)
+#--------- Load data
+data(edhec)
+colnames(edhec)=c("CA", "CTAG", "DIS", "EM",
+                  "EMN", "ED", "FIA", "GM", "L/S",
+                  "MA", "RV", "SS", "FoF")
+nboot=500
+#--------- Set random seed
+set.seed(123)
+#--------- Expected Shortfall estimates and their S.E.'s
+# h2o.init()
+edhec = edhec * 1
+scale = FALSE
+if(scale){
+  edhec.sigmas = apply(edhec, 2, sd)
+  edhec = apply(edhec, 2, function(x) (x - mean(x))/sd(x))
+}
+
+res.ES=ES.SE(edhec, p=.95, method="historical",nsim = nboot,
+             se.method = c("IFiid","IFcor","BOOTiid","BOOTcor"),
+             standardize = FALSE)
+
+res.ES.df = printSE(res.ES, round.digit = 3)
+
+res.SR=SharpeRatio.SE(edhec, p=.95, method="historical",nsim = nboot,
+                      se.method = c("IFiid","IFcor","BOOTiid","BOOTcor"))
+
+printSE(res.SR, round.digit = 3)
+
+###### simulation test
+tmp = xts(matrix(rnorm(1500, 0.05, 0.2), nrow = 100), Sys.Date() - seq(100, 1))
+standardize = function(x){
+  return(x.standardized = (x - mean(x))/sd(x), sigma = sd(x))
+}
+
+tmp.standardized = apply(tmp, 2, function(x) (x - mean(x))/sd(x))
+tmp.sigmas = apply(tmp, 2, sd)
+
+res.ES = ES.SE(R = tmp, p=.95, method="historical",nsim = nboot,
+      se.method = c("IFiid","IFcor","BOOTiid","BOOTcor"))
+printSE(res.ES)
+
+res.ES.standardized = ES.SE(R = tmp.standardized, p=.95, method="historical",nsim = nboot,
+                            se.method = c("IFiid","IFcor","BOOTiid","BOOTcor"))
+printSE(res.ES.standardized)
+
+apply(edhec, 2, mean)
+apply(edhec, 2, sd)
+
+(dd <- data.frame(x = 1:8, f = gl(2,4), ch = I(letters[1:8])))
+# print() with defaults
+print(dd, quote = TRUE, row.names = FALSE)
+# suppresses row.names and quotes all entries
+
+d=7
+alpha=0.5
+keep=1
+data = ES.IF(coredata(edhec[,1]))
+
+res = myperiodogram(tmp.IF)
+plot(my.freq, my.periodogram, type = "l")
+
+
