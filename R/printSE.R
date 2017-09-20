1ad0a70c4ceb8f59e464b9da80ab217cfcea51b5
Xin Chen <chenx26@uw.edu>
2017-09-20 15:44:14 -0700

    changed returnval option to valonly option

--- R/printSE.R
+++ R/printSE.R
@@ -1,9 +1,10 @@
 #' Formatted output of the results from xxx.SE() functions
 #'
 #' @param res the results from the xx.SE() functions
+#' @param valonly if true, return only the data frame, otherwise print and return data frame
 #' @param round.digit the number of digits to round to
 #'
-#' @return No return values
+#' @return data frame
 #' @export
 #' @author Xin Chen, \email{chenx26@uw.edu}
 #'
@@ -11,7 +12,7 @@
 #' data(edhec)
 #' res = ES.SE(edhec, p=.95, method="historical",se.method = "IFiid")
 #' printSE(res, round.digit = 4)
-printSE = function(res , round.digit = 3){
+printSE = function(res , round.digit = 3, valonly = TRUE){
   N = length(res)
   # if(N != 2) {
   #   cat("the results do not contain standard errors!\n")
@@ -25,8 +26,11 @@ printSE = function(res , round.digit = 3){
   colnames(res.df) = list.names
   rownames(res.df) = colnames(res[[1]])
 #  res.df = round(res.df, digits = round.digit)
+  if(valonly){
+    return(res.df)
+  }
   print(res.df, digits = round.digit)
   # res.df[2] = paste("(",res.df[,2],")",sep="")
   # res.df[,-1] = apply(as.data.frame(res.df[,-1]),2,function(x) paste("(",x,")",sep=""))
-  return(res.df)
+
 }
